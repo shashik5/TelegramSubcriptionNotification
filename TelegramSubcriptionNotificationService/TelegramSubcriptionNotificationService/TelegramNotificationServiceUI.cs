c3c844c35caa34fb2116323133dbad1d085f69e3
Shashikumar D <scorpion.sk5@hotmail.com>
2017-10-06 19:23:20 +0530

    First check

---
+++ TelegramSubcriptionNotificationService/TelegramSubcriptionNotificationService/TelegramNotificationServiceUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramSubcriptionNotificationService
+{
+    public partial class TelegramNotificationServiceUI : ServiceBase
+    {
+        public TelegramNotificationServiceUI()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void panel1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
